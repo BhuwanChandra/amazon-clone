de976cdfa3c56b0d7e65f8584d39d08dd1ccd204
BhuwanChandra <bhuwan7779@gmail.com>
2020-07-16 15:55:04 +0530

    created subtotal component

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,6 +1,35 @@
 export const initialState = {
-  basket: []
+  basket: [
+    {
+      id: "1234assd",
+      title:
+        "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses by Eric Ries",
+      price: 11.95,
+      rating: 5,
+      image: "https://m.media-amazon.com/images/I/51PAIR77wJL._SL500_.jpg"
+    },
+    {
+      id: "123df4",
+      title:
+        "The Lean Startup",
+      price: 15.95,
+      rating: 5,
+      image: "https://m.media-amazon.com/images/I/51PAIR77wJL._SL500_.jpg"
+    },
+    {
+      id: "12sdfs34",
+      title:
+        "How Constant Innovation Creates Radically Successful Businesses by Eric Ries",
+      price: 31.95,
+      rating: 5,
+      image: "https://m.media-amazon.com/images/I/51PAIR77wJL._SL500_.jpg"
+    }
+  ],
+  user: null,
 };
+
+export const getBasketTotal = (basket) => 
+basket && basket.reduce((amount, item) => item.price + amount, 0);
 
 function reducer(state, action) {
   console.log(action);
@@ -11,9 +40,20 @@ function reducer(state, action) {
         basket: [...state.basket, action.item]
       };
     case "REMOVE_FROM_BASKET":
+      let newBasket = [...state.basket];
+      const index = state.basket.findIndex(
+        basketItem => basketItem.id === action.id
+      );
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in the basket!!`
+        );
+      }
       return {
         ...state,
-        basket: []
+        basket: newBasket
       };
 
     default:
